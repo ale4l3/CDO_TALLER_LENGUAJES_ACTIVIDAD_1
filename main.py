60cbe49680260ecd85396c3202b9eddc75672306
ale4l3 <alejandromanuelamor@gmail.com>
2025-03-19 12:47:25 -0300

    Se agrego un input para configurar la longitud de la contraseña

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ print("Generador de claves!")
 chars = string.ascii_letters + string.digits + string.punctuation
 
 password = ""
-length = 10
+length = int(input("Ingrese la longitud de la contraseña: "))
 
 for _ in range(length):
     password = password + random.choice(chars)
