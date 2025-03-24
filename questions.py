b604a15a9180373a05818cd9825bdefb2e2e8bb8
ale4l3 <alejandromanuelamor@gmail.com>
2025-03-24 11:01:54 -0300

    MODIFICACION 3 del punto 7. Ahora no se utilizan los indices para acceder a la preguntas sino que se usa una lista de tuplas en un trio ordenado y seleccionada de manera aleatoria

--- questions.py
+++ questions.py
@@ -24,14 +24,14 @@ answers = [
 correct_answers_index = [1, 2, 0, 3, 1]
 # Se inicializa el puntaje en 0
 puntaje = 0
+#Se combina las tres listas en una lista de tuplas y se elige 3 elementos aleatorios de la lista combinada, teniendo un trio ordenado por pregunta,respuestas y respuesta correcta
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+for question, answers_options, correct_answer in questions_to_ask:
 
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(question)
+    for i, answer in enumerate(answers_options):
         print(f"{i + 1}. {answer}")
 
 # El usuario tiene 2 intentos para responder correctamente
@@ -44,7 +44,7 @@ for _ in range(3):
         else:
             user_answer = int(user_answer) - 1
         # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
+        if user_answer == correct_answer:
             print("¡Correcto!")
             puntaje += 1
             break
@@ -52,8 +52,7 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index]
-        [correct_answers_index[question_index]])
+        print(answers_options[correct_answer])
         puntaje -= 0.5
 
     # Se imprime un blanco al final de la pregunta
